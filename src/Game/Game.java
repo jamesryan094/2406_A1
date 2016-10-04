800d851d723ee99353287e5b34f676ecda9b22bf
jamesryan094 <james.ryan@my.jcu.edu.au>
2016-10-04 13:28:16 +1000

    Refactored Game.java in Game package to have logical grouping/ordering of method decelerations. Documentation added for Class and larger methods in class. All intelliJ warnings now absent from file

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -1,5 +1,7 @@
 package Game;
+
 import Cards.Card;
+
 import Deck.Deck;
 import Deck.GenerateDeckFromPLIST;
 
@@ -11,43 +13,17 @@ import java.util.ArrayList;
 import java.util.Random;
 
 /**
- *
+ * Instantiates Game object that represents the state of any Mineral Supertrumps Game.
+ * Attributes are manipulated through method calls from an adjacent "controller" file; RunGame.
+ * Class also stores large string to be printed in the Game's menu and String constants.
  *
  * Created by james on 6/09/2016.
  */
 class Game {
-    private static final int NUM_CARDS_PER_HAND = 8;
-    static final String TRUMP_HIERARCHIES = "Information on trump categories:\n\n" +
-            "Hardness: relates to Moh’s hardness scale of minerals from 1 to 10. Where a\n" +
-            "range of values is presented, the highest value should be used.\n\n" +
-            "Specific Gravity: in grams per cubic centimeter. Where a range of values is\n" +
-            "presented, the highest value should be used.\n\n" +
-            "Cleavage: refers to the number of cleavage planes and how well the planes\n" +
-            "are typically expressed in the crystal. For example, “1 perfect, 2 poor” means\n" +
-            "the mineral has 1 perfect cleavage plane, and 2 poor cleavage planes. The\n" +
-            "order of ranking from lowest to highest is:\n" +
-            "none < poor/none < 1 poor < 2 poor < 1 good < 1 good, 1 poor < 2\n" +
-            "good < 3 good < 1 perfect < 1 perfect, 1 good < 1 perfect, 2 good < 2\n" +
-            "perfect, 1 good < 3 perfect < 4 perfect < 6 perfect.\n\n" +
-            "Crustal Abundance: is ranked from lowest to highest as:\n" +
-            "ultratrace < trace < low < moderate < high < very high.\n\n" +
-            "Economic Value: is ranked from lowest to highest as:\n" +
-            "trivial < low < moderate < high < very high < I’m rich!\n";
-    private final Deck deck;
+
     final static int MIN_PLAYERS = 3;
     final static int MAX_PLAYERS = 5;
-    private Player[] players;
-    private int numPlayers;
-    private static int dealerIndex;
-    private Player currentPlayer;
-    private boolean isWon;
-    private boolean humanPlayedCard = false;
-    private Card lastPlayedCard;
-    private String currentTrumpCategory;
-    private int numPasses;
-    private ArrayList<Player> winners = new ArrayList<>();
-    private boolean isFirstTurn;
-    private boolean comboPlayed;
+    private static final int NUM_CARDS_PER_HAND = 8;
     final static String INSTRUCTIONS = "How to play:\n" +
             "1. A dealer (randomly chosen) shuffles the cards and deals each player 8\n" +
             "cards. Each player can look at their cards, but should not show them to\n" +
@@ -83,8 +59,51 @@ class Game {
             "chooses the trump category to be played.\n\n" +
             "7. The winner of the game is the first player to lose all of their cards. The\n" +
             "game continues until all but one player (i.e., the loser) has lost their cards.\n";
+    static final String TRUMP_HIERARCHIES = "Information on trump categories:\n\n" +
+            "Hardness: relates to Moh’s hardness scale of minerals from 1 to 10. Where a\n" +
+            "range of values is presented, the highest value should be used.\n\n" +
+            "Specific Gravity: in grams per cubic centimeter. Where a range of values is\n" +
+            "presented, the highest value should be used.\n\n" +
+            "Cleavage: refers to the number of cleavage planes and how well the planes\n" +
+            "are typically expressed in the crystal. For example, “1 perfect, 2 poor” means\n" +
+            "the mineral has 1 perfect cleavage plane, and 2 poor cleavage planes. The\n" +
+            "order of ranking from lowest to highest is:\n" +
+            "none < poor/none < 1 poor < 2 poor < 1 good < 1 good, 1 poor < 2\n" +
+            "good < 3 good < 1 perfect < 1 perfect, 1 good < 1 perfect, 2 good < 2\n" +
+            "perfect, 1 good < 3 perfect < 4 perfect < 6 perfect.\n\n" +
+            "Crustal Abundance: is ranked from lowest to highest as:\n" +
+            "ultratrace < trace < low < moderate < high < very high.\n\n" +
+            "Economic Value: is ranked from lowest to highest as:\n" +
+            "trivial < low < moderate < high < very high < I’m rich!\n";
 
+    private Deck deck;
 
+    private Card lastPlayedCard;
+    private String currentTrumpCategory;
+
+    private Player[] players;
+    private Player currentPlayer;
+    private ArrayList<Player> winners = new ArrayList<>();
+
+    private int numPasses;
+    private int numPlayers;
+    private static int dealerIndex;
+
+    private boolean isFirstTurn;
+    private boolean humanPlayedCard = false;
+    private boolean comboPlayed;
+    private boolean isWon;
+
+    /**
+     * Game constructor takes the desired number of players for the game, as well as
+     * a User's username, and creates the appropriate number of human and nonHuman players.
+     * The userName is assigned to the human player's name field.
+     * Relevant values are initialised to ensure the first turn has its appropriate conditions followed.
+     *
+     *
+     * @param num represent the desired number of players for the current game's party.
+     * @param userName is used to identify the human player/alert the user to their being up.
+     */
     Game(int num, String userName){
         this.deck = GenerateDeckFromPLIST.buildDeck();
         numPlayers = num;
@@ -95,6 +114,13 @@ class Game {
         comboPlayed = false;
     }
 
+    /**
+     * Accesses the numPlayers field to determine the number of player objects to make,
+     * returns a Player array with this many players, one of them being a human player,
+     * with the passed username as their name.
+     * @param userName to be stored in human players name field.
+     * @return Static array of players in the game.
+     */
     private Player[] generatePlayers(String userName) {
         Player[] playerArray =  new Player[numPlayers];
         playerArray[0] = new HumanPlayer(userName);
@@ -104,13 +130,23 @@ class Game {
         return playerArray;
     }
 
+    /**
+     * Generates random number within range (0 - numPlayers, (inclusive)),
+     * assigns the player at this index in Players array as the dealer for the game.
+     */
     void assignDealer(){
         Random rn = new Random();
         dealerIndex = rn.nextInt(numPlayers);
         players[dealerIndex].setIsDealer(true);
     }
 
-
+    /**
+     * The rule cards are first removed from the deck and the deck is shuffled.
+     * The NUM_CARDS_PER_HAND constant is accessed to determine how many cards to deal,
+     * the Player array field is accessed to determine who to deal to.
+     * Dealing involves removing the a Card from the game's deck attribute
+     * and placing it in the appropriate Player's hand attribute.
+     */
     void initialDeal(){
         deck.removeRuleCards();
         deck.shuffle();
@@ -130,10 +166,16 @@ class Game {
             ArrayList<Card> hand= deck.dealHand(NUM_CARDS_PER_HAND);
             player.setCurrentHand(hand);
         }
+
+//        Dealer set as current player so that "player to the left of the dealer" goes first, as per the rules.
         this.currentPlayer = players[dealerIndex];
     }
 
-
+    /**
+     * Sets the current player to the next person in the Player array.
+     * If the previous player was the last in the array, the current player is reset to the first.
+     * @return currentPlayer; the player who is to play the current turn.
+     */
     Player incrementCurrentPlayer() {
         int currentPlayerId = currentPlayer.getId();
         if (currentPlayerId == players.length - 1){
@@ -165,6 +207,10 @@ class Game {
         return this.getCurrentPlayer().isHuman();
     }
 
+    /**
+     * Check if the human has played a card to determine of the user turn menu should keep running.
+     * @return true if human played a card and wishes to end their turn, otherwise false
+     */
     boolean getHumanPlayedCard(){
         return humanPlayedCard;
     }
@@ -187,69 +233,6 @@ class Game {
 
     String getCurrentTrumpCategory() {
         return currentTrumpCategory;
-    }
-
-    private void setCurrentTrumpCategory(String currentTrumpCategory) {
-        this.currentTrumpCategory = currentTrumpCategory;
-    }
-
-    void playFirstTurn(int cardChoice, String trumpChoice) {
-//        When a user is playing a MineralCard Or 'The Geologist':
-        Card chosenCard = currentPlayer.playCard(cardChoice);
-        setCurrentTrumpCategory(trumpChoice);
-        setLastPlayedAttributes(chosenCard);
-        setHumanPlayedCard();
-    }
-
-    void playTrumpCard(int cardChoice) {
-//        When a user plays a Trump Card:
-        Card chosenCard = currentPlayer.playCard(cardChoice);
-        String trumpChoice = getTrumpChoiceFromTrumpCard(chosenCard.getTitle());
-        setCurrentTrumpCategory(trumpChoice);
-        setLastPlayedAttributes(chosenCard);
-        setHumanPlayedCard();
-        resetRoundTrump();
-    }
-
-//    public void playTrumpCard() {
-////    When a robot plays first:
-//
-//        Card cardChoice = currentPlayer.playAnyMineralCard();
-//        String trumpChoice;
-//        if(cardChoice.getCardType().equals("play")||cardChoice.isGeologist()){
-//            trumpChoice = currentPlayer.getTrumpCategoryChoice();
-//        }
-//        else{
-//            trumpChoice = getTrumpChoiceFromTrumpCard(cardChoice.getTitle());
-//        }
-//        setCurrentTrumpCategory(trumpChoice);
-//        setLastPlayedAttributes(cardChoice);
-//        setCardHasBeenPlayed(true);
-//
-//    }
-    void playFirstTurn(){
-//        When a robot plays first card of round:
-        String trumpChoice;
-        Card cardChoice;
-        resetComboPlayed();
-        //first turn of game
-        if (isFirstTurn){
-            cardChoice = currentPlayer.playAnyMineralCard();
-            trumpChoice = currentPlayer.getTrumpCategoryChoice();
-        }
-        //first turn of ROUND
-        else {
-            cardChoice = currentPlayer.playAnyCard();
-            if (cardChoice.getCardType().equals("play") || cardChoice.isGeologist()){
-                trumpChoice = currentPlayer.getTrumpCategoryChoice();
-            }
-            else {
-                trumpChoice = getTrumpChoiceFromTrumpCard(cardChoice.getTitle());
-            }
-        }
-        setCurrentTrumpCategory(trumpChoice);
-        setLastPlayedAttributes(cardChoice);
-
     }
 
     private String getTrumpChoiceFromTrumpCard(String trumpTitle){
@@ -278,7 +261,76 @@ class Game {
         return trumpChoice;
     }
 
+    private void setCurrentTrumpCategory(String currentTrumpCategory) {
+        this.currentTrumpCategory = currentTrumpCategory;
+    }
 
+    /**
+     * Handles the decision process for a nonHuman player playing the first turn of a round or the game.
+     * will attempt to play combo, if it can not then any playable card.
+     * The attributes of the card in play are updated appropriately.
+     */
+    void playFirstTurn(){
+//        When a robot plays first card of round:
+        String trumpChoice;
+        Card cardChoice;
+        resetComboPlayed();
+        //first turn of game
+        if (isFirstTurn){
+            cardChoice = currentPlayer.playAnyMineralCard();
+            trumpChoice = currentPlayer.getTrumpCategoryChoice();
+        }
+        //first turn of ROUND
+        else {
+            cardChoice = currentPlayer.playAnyCard();
+            if (cardChoice.getCardType().equals("play") || cardChoice.isGeologist()){
+                trumpChoice = currentPlayer.getTrumpCategoryChoice();
+            }
+            else {
+                trumpChoice = getTrumpChoiceFromTrumpCard(cardChoice.getTitle());
+            }
+        }
+        setCurrentTrumpCategory(trumpChoice);
+        setLastPlayedAttributes(cardChoice);
+
+    }
+
+    /**
+     * Called only when a user is the first player of a round and is playing a mineral card or "The Geologist".
+     * Prompts the user for a trump category choice then updated the value and category in play.
+     * @param cardChoice the card from which the trump value is taken.
+     * @param trumpChoice determines which of the cards attributes to use as a play value.
+     */
+    void playFirstTurn(int cardChoice, String trumpChoice) {
+//        When a user is playing a MineralCard Or 'The Geologist':
+        Card chosenCard = currentPlayer.playCard(cardChoice);
+        setCurrentTrumpCategory(trumpChoice);
+        setLastPlayedAttributes(chosenCard);
+        setHumanPlayedCard();
+    }
+
+    /**
+     * Called only when a user plays a trump card that is not "The Geologist".
+     * Method will set the current trump category based on card title.
+     * @param cardChoice the trump card from which the trump category in play is determined.
+     */
+    void playTrumpCard(int cardChoice) {
+//        When a user plays a Trump Card:
+        Card chosenCard = currentPlayer.playCard(cardChoice);
+        String trumpChoice = getTrumpChoiceFromTrumpCard(chosenCard.getTitle());
+        setCurrentTrumpCategory(trumpChoice);
+        setLastPlayedAttributes(chosenCard);
+        setHumanPlayedCard();
+        resetRoundTrump();
+    }
+
+    /**
+     * Handles the decision process for a nonHuman player playing any turn that
+     * is not the first turn of a round or the game.
+     * will attempt to play combo, if it can not then any playable card.
+     * The attributes of the card in play are updated appropriately.
+     * If the player has no playable cards, they pass.
+     */
     void playTurn() {
         if (currentPlayer.hasCombo()){
             playCombo();
@@ -308,9 +360,11 @@ class Game {
 
     }
 
+    /**
+     * Increments the current player and has player draw a card from deck if the deck is not empty.
+     */
     void passTurn() {
         currentPlayer.setHasPassed(true);
-//        removePlayerFromRound();
         if((deck.getCards().size() != 0)&&currentPlayer.getHand().size()!=0) {
             currentPlayer.drawCard(deck);
         }
@@ -324,9 +378,11 @@ class Game {
         numPasses +=1 ;
     }
 
+    /**
+     * Resets the number of passes and replaces players who hav passes and not yet won
+     */
     void resetRound() {
         resetNumPasses();
-//        resetPlayersInRound();
         for (Player player:players){
             if(!winners.contains(player)) {
                 player.setHasPassed(false);
@@ -334,24 +390,15 @@ class Game {
         }
     }
 
+    /**
+     * Replaces the players in the round after someone plays a Supertrump card
+     */
     void resetRoundTrump() {
         resetNumPasses();
-//        resetPlayersInRound();
         for (Player player:players){
             player.setHasPassed(false);
         }
     }
-
-//    private void removePlayerFromRound() {
-//        playersInRound.remove(currentPlayer);
-//    }
-
-//    private void resetPlayersInRound(){
-//        playersInRound.clear();
-//        for(int i = 0; i < getPlayers().length; ++i){
-//            playersInRound.add(getPlayers()[i]);
-//        }
-//    }
 
     boolean checkWinner() {
         return getCurrentPlayer().getHand().size() == 0;
@@ -361,40 +408,45 @@ class Game {
         return winners;
     }
 
+    /**
+     * Adds a player to the winner list.
+     * Will set value to end game if all but one player in list
+     */
     void updateWinners() {
         if(!winners.contains(currentPlayer)){
             winners.add(currentPlayer);
-//            System.out.println("Congratulations " + currentPlayer.getName() + "! You have been added to the winner list!");
-//            currentPlayer.setHasPassed(true);
-//            currentPlayer.setHasPassed(true);
         }
         if(winners.size() == getPlayers().length-1){
             setIsWon();
-//            System.out.println("Game Completed! Game over!");
         }
-//        if (winners.size() == getPlayers().length - 1){
-//            setIsWon();
-//            System.out.println("Game Completed! Game over!");
-//        }
     }
 
     private void setIsWon(){
         isWon = true;
     }
 
-    void setFirstTurn(boolean firstTurn) {
-        isFirstTurn = firstTurn;
-    }
-
     boolean isFirstTurn() {
         return isFirstTurn;
     }
 
+    void setFirstTurn(boolean firstTurn) {
+        isFirstTurn = firstTurn;
+    }
+
+    /**
+     * Checks how many of the players in the game have passed.
+     * If this number is one less than the number of players who have not already won, the round is reset.
+     * @return true if number of passes is one less than number of players.
+     */
     boolean isNewRound() {
         return (numPasses == numPlayers - 1 - winners.size());
 
     }
 
+    /**
+     * Calls appropriate Player subclass methods for removing the two combo cards from player's hand.
+     * Resets the round, as per the game rules, to have combo player go again.
+     */
     void playCombo() {
         this.lastPlayedCard = currentPlayer.playCombo();
         comboPlayed = true;
